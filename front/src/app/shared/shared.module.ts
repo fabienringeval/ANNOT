7f5118e247de584d5eebcf91bcb30cedce5208b9
fabienringeval <fabien.ringeval@gmail.com>
2025-03-18 14:40:39 +0100

    Add files via upload

--- front/src/app/shared/shared.module.ts
+++ front/src/app/shared/shared.module.ts
@@ -1,1 +1,37 @@
+import { NgModule } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ButtonModule } from './components/button/button.module';
+import { VideoPlayerComponent } from './components/video-player/video-player.component';
+import { SimplevideoPlayerComponent } from './components/simplevideo-player/simplevideo-player.component';
+import { AudioAnnotationSummaryComponent } from './components/audio-annotation-summary/audio-annotation-summary.component';
+import {SliderModule} from './components/slider/slider.module';
+import {ButtonComponent} from './components/button/button.component';
+
+@NgModule({
+    imports: [CommonModule, SliderModule, ButtonModule],
+    exports: [
+        SharedModule.MODULE_LIST,
+        VideoPlayerComponent,
+        SimplevideoPlayerComponent,
+        AudioAnnotationSummaryComponent,
+        ButtonComponent
+    ],
+    declarations: [VideoPlayerComponent, SimplevideoPlayerComponent, AudioAnnotationSummaryComponent]
+})
+
+export class SharedModule {
+
+    static readonly MODULE_LIST = [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule,
+        MatSnackBarModule,
+        ButtonModule
+    ];
+
+}
